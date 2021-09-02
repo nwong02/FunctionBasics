eeec2f23ae74e501ad5a0db3af31fe4f3122b92c
Nelson Wong <nelsonwong107@gmail.com>
2021-09-01 21:46:31 -0400

    Add project files.

---
+++ BiteRateFileSize.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+* Project		: Function Basics
+* Title			: BiteRateFileSize.cpp
+* Author		: Nelson Wong
+* Description	: A program will read in the bitrate in kilobytes per second and duration in minutes, outputs the file size of the video clip in megabytes
+*/
+
+
+#define kMegaByte 8000
+
+
+
+int main(void)
+{
+	return 0;
+}
