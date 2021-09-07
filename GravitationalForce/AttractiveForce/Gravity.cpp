13509139b8b129338c2bfcd519000fba085ac34a
Nelson Wong <nelsonwong107@gmail.com>
2021-09-06 22:32:52 -0400

    C style strings with user inputs

--- GravitationalForce/AttractiveForce/Gravity.cpp
+++ GravitationalForce/AttractiveForce/Gravity.cpp
@@ -17,14 +17,25 @@ double gravitationalForce(double bodyOne, double bodyTwo, double radialDist);
 
 double gravitationalForce(double bodyOne, double bodyTwo, double radialDist)
 {
+	double distanceSquare = pow(radialDist, 2);
+	double gravitationalForce = kGravitationalConstant * (bodyOne * bodyTwo) / distanceSquare;
+	
+	printf("The gravitational force between these two bodies is: %e\n", gravitationalForce);
 
+	return gravitationalForce;
 }
 
 
 
 int main()
 {
-	double gravitationalConstant = kGravitationalConstant;
-	printf("%e", gravitationalConstant);
+	double massOne = 0.0;
+	double massTwo = 0.0;
+	
+	printf("Enter the value of the first mass: \n");
+	printf("Enter the value of the second mass: \n");
+	scanf("%f", &massOne);
+	scanf("%f", &massTwo);
+
 	return 0;
 }
