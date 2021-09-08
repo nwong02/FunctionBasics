0957c1607ea9ccf70d822939f0dbb99eb502cc4a
Nelson Wong <nelsonwong107@gmail.com>
2021-09-07 22:57:25 -0400

    Jacket sizing needs incrementing by 10 for adjusted sizing to take effect

--- ClothingSizes/ClothingSize.cpp
+++ ClothingSizes/ClothingSize.cpp
@@ -7,3 +7,65 @@
 * Author		: Nelson Wong
 * Description	: Ask for user's height, weight, and age, and calculate their clothing sizes as appropriate based on formula. Program repeats as often as necessary
 */
+
+
+
+
+/*
+* Function		: hatSize()
+* Description	: Calculates the hat size based on weight and height in lbs and inches respectively
+* Parameters	: int weight, double height
+* Returns		: hat size, dimensionless
+*/
+
+double hatSize(int weight, double height);
+
+double hatSize(int weight, double height)
+{
+	char buffer[100] = { 0 };
+	char reffub[100] = { 0 };
+
+	double hatSize = (weight / height) * 2.9;
+
+	printf("Enter the weight in pounds: ");
+	fgets(buffer, 100, stdin);
+	weight = atoi(buffer);
+
+	printf("Enter the height in inches: ");
+	fgets(reffub, 100, stdin);
+	height = atof(reffub);
+
+	printf("Your hat size is: %g", hatSize);
+	return hatSize;
+}
+
+
+
+
+
+/*
+* Function		: jacketSize()
+* Description	: Calculates the jacket size based on weight and height in lbs and inches respectively, and the wearer's age in years. For every ten full years starting at 30, the jacket sizing will be adjusted with an additional 1/8 inch 
+* Parameters	: int weight, double height, int age
+* Returns		: jacket size, dimensionless
+*/
+
+double jacketSize(int height, double weight, int age);
+
+double jacketSize(int height, double weight, int age)
+{
+	double jacketSizing = (height * weight) / 288;
+	if (age > 30)
+	{
+		jacketSizing = jacketSizing + (1 / 8);
+		
+		printf("Your jacket size: %f", jacketSizing);
+		return jacketSizing;
+	}
+}
+
+int main()
+{
+	jacketSize(120, 53.5, 31);
+	return 0;
+}
