25df7f01322a498ec40e6c3d131d5becbdbc683b
Nelson Wong <nelsonwong107@gmail.com>
2021-09-08 22:12:45 -0400

    Corrected error with formal redefinition of variables, lines 26-30

--- DistanceFormula/Distance.cpp
+++ DistanceFormula/Distance.cpp
@@ -24,10 +24,10 @@ double distance(double x_1, double y_1, double x_2, double y_2);
 
 double distance(double x_1, double y_1, double x_2, double y_2)
 {
-	double x_1 = 0.0;
-	double y_1 = 0.0;
-	double x_2 = 0.0;
-	double y_2 = 0.0;
+	x_1 = 0.0;
+	y_1 = 0.0;
+	x_2 = 0.0;
+	y_2 = 0.0;
 
 	char xOne[10] = { 0 };
 	char yOne[10] = { 0 };
@@ -48,10 +48,10 @@ double distance(double x_1, double y_1, double x_2, double y_2)
 
 	printf("Enter any point for y_2: ");
 	fgets(yTwo, 10, stdin);
-	x_1 = atof(yTwo);
+	y_2 = atof(yTwo);
 
 	double distance = sqrt(pow(x_2 - x_1, 2) + pow(y_2 - y_1, 2) * 1.0);
-	printf("The distance between (%f, %f) and (%f, %f)is: %f", x_1, y_1, x_2, y_2, distance);
+	printf("The distance between (%f, %f) and (%f, %f) is: %f \n", x_1, y_1, x_2, y_2, distance);
 
 	return distance;
 }
@@ -59,5 +59,6 @@ double distance(double x_1, double y_1, double x_2, double y_2)
 
 int main()
 {
+	distance(2, 2, 5, 5);
 	return 0;
 }
